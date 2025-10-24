c2d5b38698ccde9390140b553179120ba425167a
farrellzidane <raihandrawan@gmail.com>
2025-10-24 17:42:14 +0700

    Initial commit

---
+++ src/main/java/apap/ti/_5/accommodation_2306275600_be/Accommodation2306275600BeApplication.java
@@ -0,0 +1,13 @@
+package apap.ti._5.accommodation_2306275600_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accommodation2306275600BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accommodation2306275600BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/_5/accommodation_2306275600_be/Accommodation2306275600BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.accommodation_2306275600_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accommodation2306275600BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
