ab9fd85e1eabaf57cd0a8ec0bbc97dd5364f5b34
farrellzidane <raihandrawan@gmail.com>
2025-11-07 13:24:59 +0700

    Setup Test

---
+++ src/test/java/apap/ti/_5/accommodation_2306275600_be/restdto/response/booking/BookingDetailResponseDTOTest.java
@@ -0,0 +1,5 @@
+package apap.ti._5.accommodation_2306275600_be.restdto.response.booking;
+
+public class BookingDetailResponseDTOTest {
+    
+}
---
+++ src/test/java/apap/ti/_5/accommodation_2306275600_be/restdto/response/property/RoomTypeInfoDTOTest.java
@@ -0,0 +1,5 @@
+package apap.ti._5.accommodation_2306275600_be.restdto.response.property;
+
+public class RoomTypeInfoDTOTest {
+    
+}
