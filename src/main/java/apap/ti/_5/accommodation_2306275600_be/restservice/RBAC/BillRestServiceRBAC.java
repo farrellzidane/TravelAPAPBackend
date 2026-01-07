f8adb4bd2c9987eca31f0683af102ec1a6869791
farrellzidane <raihandrawan@gmail.com>
2026-01-08 01:55:47 +0700

    feat: Extend BillRestServiceRBAC with additional methods for enhanced bill management

--- src/main/java/apap/ti/_5/accommodation_2306275600_be/restservice/RBAC/BillRestServiceRBAC.java
+++ src/main/java/apap/ti/_5/accommodation_2306275600_be/restservice/RBAC/BillRestServiceRBAC.java
@@ -1,5 +1,6 @@
 package apap.ti._5.accommodation_2306275600_be.restservice.RBAC;
 
+import apap.ti._5.accommodation_2306275600_be.exceptions.AccessDeniedException;
 import apap.ti._5.accommodation_2306275600_be.restdto.request.bill.CreateBillRequestDTO;
 import apap.ti._5.accommodation_2306275600_be.restdto.request.bill.PayBillRequestDTO;
 import apap.ti._5.accommodation_2306275600_be.restdto.request.bill.UpdateBillRequestDTO;
@@ -10,12 +11,20 @@ import apap.ti._5.accommodation_2306275600_be.restservice.BillRestService;
 import java.util.List;
 import java.util.UUID;
 
+
 public interface BillRestServiceRBAC extends BillRestService {
-    BillResponseDTO createBill(CreateBillRequestDTO dto);
-    List<BillResponseDTO> getAllBills(UUID customerId, String serviceName, Integer status);
-    List<BillResponseDTO> getCustomerBills(UUID customerId, Integer status, String sortBy, String sortDirection);
-    List<BillResponseDTO> getServiceBills(String serviceName, Integer status, UUID customerId);
-    BillDetailResponseDTO getBillDetail(UUID billId);
-    BillResponseDTO payBill(UUID billId, PayBillRequestDTO dto, UUID authenticatedCustomerId);
-    BillResponseDTO updateBill(UUID billId, UpdateBillRequestDTO dto);
+    @Override
+    BillResponseDTO createBill(CreateBillRequestDTO dto) throws AccessDeniedException;;
+    @Override
+    List<BillResponseDTO> getAllBills(UUID customerId, String serviceName, Integer status) throws AccessDeniedException;;
+    @Override
+    List<BillResponseDTO> getCustomerBills(UUID customerId, Integer status, String sortBy, String sortDirection) throws AccessDeniedException;;
+    @Override
+    List<BillResponseDTO> getServiceBills(String serviceName, Integer status, UUID customerId) throws AccessDeniedException;;
+    @Override
+    BillDetailResponseDTO getBillDetail(UUID billId) throws AccessDeniedException;;
+    @Override
+    BillResponseDTO payBill(UUID billId, PayBillRequestDTO dto, UUID authenticatedCustomerId) throws AccessDeniedException;;
+    @Override
+    BillResponseDTO updateBill(UUID billId, UpdateBillRequestDTO dto) throws AccessDeniedException;;
 }
